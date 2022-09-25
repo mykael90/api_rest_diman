67e6fca9045d813c1ff7ef354d6ed301cdbc5d14
Mykael Mello <mykael.mello@ufrn.br>
2022-09-25 18:18:05 -0300

    avanços

--- src/models/MaterialInventory.js
+++ src/models/MaterialInventory.js
@@ -1,4 +1,4 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize, { Model, DECIMAL } from 'sequelize';
 
 export default class MaterialInventory extends Model {
   static associate(models) {
@@ -14,26 +14,26 @@ export default class MaterialInventory extends Model {
       },
 
       initialQuantity: {
-        type: Sequelize.DECIMAL,
+        type: Sequelize.DECIMAL(10, 2),
         allowNull: false,
         defaultValue: 0,
       },
 
       releaseInventory: {
-        type: Sequelize.DECIMAL,
+        type: Sequelize.DECIMAL(10, 2),
         allowNull: true,
         defaultValue: 0,
       },
       restrictInventory: {
-        type: Sequelize.DECIMAL,
+        type: Sequelize.DECIMAL(10, 2),
         allowNull: true,
         defaultValue: 0,
       },
       freeInventory: {
         type: Sequelize.VIRTUAL,
         get() {
-          return Number(this.initialQuantity)
-          + Number(this.releaseInventory);
+          return (Number(this.initialQuantity)
+          + Number(this.releaseInventory)).toFixed(2);
         },
         set(value) {
           throw new Error('Do not try to set the `total` value!');
@@ -42,9 +42,9 @@ export default class MaterialInventory extends Model {
       totalInventory: {
         type: Sequelize.VIRTUAL,
         get() {
-          return Number(this.initialQuantity)
+          return (Number(this.initialQuantity)
           + Number(this.releaseInventory)
-          + Number(this.restrictInventory);
+          + Number(this.restrictInventory)).toFixed(2);
         },
         set(value) {
           throw new Error('Do not try to set the `total` value!');
