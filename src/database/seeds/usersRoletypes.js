036c275894c241eaf3a5965a4ef8e4a18364484a
Mykael Mello <mykael.mello@ufrn.br>
2022-10-09 11:12:54 -0300

    up

--- src/database/seeds/usersRoletypes.js
+++ src/database/seeds/usersRoletypes.js
@@ -1,5 +1,13 @@
 const data = require('../JSON/roleTypesJSON.json');
 
+const date = new Date();
+const dateMariaDB = `${date.getUTCFullYear()}-${
+  (`00${date.getUTCMonth() + 1}`).slice(-2)}-${
+  (`00${date.getUTCDate()}`).slice(-2)} ${
+  (`00${date.getUTCHours()}`).slice(-2)}:${
+  (`00${date.getUTCMinutes()}`).slice(-2)}:${
+  (`00${date.getUTCSeconds()}`).slice(-2)}`;
+
 function removeAccent(text) {
   text = text.toUpperCase();
   text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'A');
@@ -12,6 +20,9 @@ function removeAccent(text) {
 }
 
 data.forEach((obj) => {
+  obj.description = removeAccent(obj.description);
+  obj.created_at = dateMariaDB;
+  obj.updated_at = dateMariaDB;
 });
 
 module.exports = {
