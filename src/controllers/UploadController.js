7744e6aab336514a501508fce338e8dd7d273e90
Mykael Mello <eng.mykael@gmail.com>
2023-08-24 04:47:38 -0300

    ok

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -10,6 +10,9 @@ const uploadPath = {
   material: resolve(__dirname, '..', '..', 'uploads', 'materials', 'images'),
   materialIn: resolve(__dirname, '..', '..', 'uploads', 'materials', 'in', 'images'),
   materialOut: resolve(__dirname, '..', '..', 'uploads', 'materials', 'out', 'images'),
+  car: resolve(__dirname, '..', '..', 'uploads', 'cars', 'images'),
+  carInspection: resolve(__dirname, '..', '..', 'uploads', 'cars', 'inspections', 'images'),
+  carOccurrence: resolve(__dirname, '..', '..', 'uploads', 'cars', 'occurrence', 'images'),
 };
 
 class UploadController {
@@ -127,6 +130,108 @@ class UploadController {
         console.log('file uploaded');
       }));
 
+      return res.json(req.result);
+    } catch (e) {
+      console.log('erroCustomizado', e);
+      return res.status(400).json({
+        errors: [e.message],
+      });
+    }
+  }
+
+  async storeCar(req, res) {
+    try {
+      const dimensionResized = 800;
+
+      console.log('no FINAL', req.files);
+
+      // RESIZE WITH SHARP
+      await Promise.all(req.files.map(async (file) => {
+        console.log(12456);
+        file.buffer = await sharp(file.buffer)
+          .resize(dimensionResized, dimensionResized, {
+            fit: 'inside', // both sides must be lower than 'dimensionResized'
+            // eslint-disable-next-line max-len
+            withoutEnlargement: true, // if image's original width or height is less than specified width and height, sharp will do nothing(i.e no enlargement)
+          })
+          .withMetadata()
+          .toBuffer();
+      }));
+
+      // SAVE FILES IN PATH
+      await Promise.all(req.files.map(async (file) => {
+        // const fileName = random_5();
+        console.log('CAMINHO:', `${uploadPath.car}/${file.newName}`);
+        fs.writeFileSync(`${uploadPath.car}/${file.newName}`, file.buffer);
+      }));
+      return res.json(req.result);
+    } catch (e) {
+      console.log('erroCustomizado', e);
+      return res.status(400).json({
+        errors: [e.message],
+      });
+    }
+  }
+
+  async storeCarInspection(req, res) {
+    try {
+      const dimensionResized = 800;
+
+      console.log('no FINAL', req.files);
+
+      // RESIZE WITH SHARP
+      await Promise.all(req.files.map(async (file) => {
+        console.log(12456);
+        file.buffer = await sharp(file.buffer)
+          .resize(dimensionResized, dimensionResized, {
+            fit: 'inside', // both sides must be lower than 'dimensionResized'
+            // eslint-disable-next-line max-len
+            withoutEnlargement: true, // if image's original width or height is less than specified width and height, sharp will do nothing(i.e no enlargement)
+          })
+          .withMetadata()
+          .toBuffer();
+      }));
+
+      // SAVE FILES IN PATH
+      await Promise.all(req.files.map(async (file) => {
+        // const fileName = random_5();
+        console.log('CAMINHO:', `${uploadPath.carInspection}/${file.newName}`);
+        fs.writeFileSync(`${uploadPath.carInspection}/${file.newName}`, file.buffer);
+      }));
+      return res.json(req.result);
+    } catch (e) {
+      console.log('erroCustomizado', e);
+      return res.status(400).json({
+        errors: [e.message],
+      });
+    }
+  }
+
+  async storeCarOccurrence(req, res) {
+    try {
+      const dimensionResized = 800;
+
+      console.log('no FINAL', req.files);
+
+      // RESIZE WITH SHARP
+      await Promise.all(req.files.map(async (file) => {
+        console.log(12456);
+        file.buffer = await sharp(file.buffer)
+          .resize(dimensionResized, dimensionResized, {
+            fit: 'inside', // both sides must be lower than 'dimensionResized'
+            // eslint-disable-next-line max-len
+            withoutEnlargement: true, // if image's original width or height is less than specified width and height, sharp will do nothing(i.e no enlargement)
+          })
+          .withMetadata()
+          .toBuffer();
+      }));
+
+      // SAVE FILES IN PATH
+      await Promise.all(req.files.map(async (file) => {
+        // const fileName = random_5();
+        console.log('CAMINHO:', `${uploadPath.carOccurrence}/${file.newName}`);
+        fs.writeFileSync(`${uploadPath.carOccurrence}/${file.newName}`, file.buffer);
+      }));
       return res.json(req.result);
     } catch (e) {
       console.log('erroCustomizado', e);
